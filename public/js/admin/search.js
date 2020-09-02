d52501d31e0190aff2e6421ced8836b0a8a1db8b
Maciek <Wchodz3WTo@gmail.com>
2020-09-02 14:35:15 +0200

    Search fixed

--- public/js/admin/search.js
+++ public/js/admin/search.js
@@ -3,13 +3,30 @@ const search = document.getElementsByClassName("admin-search-inp")[0];
 
 search.onkeyup = () => {
   Array.from(searchVals).forEach((elem) => {
-      //TODO: Fix search bug
-      //TODO: Hide section label if section gets empty
-    console.log(search.value.toLowerCase(), elem.);
-    elem.parentElement.style.display = !elem.innerText
-      .toLowerCase()
-      .includes(search.value.toLowerCase())
-      ? "none"
-      : "flex";
+    elem.style.display =
+      elem.children[0].innerText
+        .toLowerCase()
+        .includes(search.value.toLowerCase()) ||
+      elem.children[1].innerText
+        .toLowerCase()
+        .includes(search.value.toLowerCase())
+        ? "flex"
+        : "none";
+
+    if (window.location.pathname == "/admin/users") {
+      if (!elem.nextSibling.nextSibling) {
+        let hidden = 0;
+        Array.from(elem.parentElement.children).forEach((child) => {
+          if (
+            child.classList.contains("search-field") &&
+            child.getAttribute("style") == "display: none;"
+          ) {
+            hidden++;
+          }
+        });
+        elem.parentElement.children[0].style.display =
+          hidden == elem.parentElement.children.length - 1 ? "none" : "flex";
+      }
+    }
   });
 };
